e79f3b5374244c6d52b08ec33751e914baf28700
Andrey Ivanov <prioritet2503@gmail.com>
2022-07-01 15:32:02 +0300

    add commit, add name title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import MovieApiService from './js/movieFetch.js';
 
+// для примера - через экземпляр можем достучаться до свойства
 const popularMovie = new MovieApiService();
 
+// вызываем промис в формате JSON
 popularMovie.fetchPopular();
