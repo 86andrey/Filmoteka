34fc8b3f38ca5c7686145f20904988f86b85552e
Nastya Lutsko <nastyalutsko777@gmail.com>
2022-07-07 15:18:51 +0200

    storage

--- src/js/renderFunctions.js
+++ src/js/renderFunctions.js
@@ -1,4 +1,5 @@
 import MovieApiService from './movieFetch';
+import { storage } from './storage';
 const movie = new MovieApiService();
 
 const containerCard = document.querySelector('.container-card')
@@ -110,19 +111,84 @@ export async function openModal(movieId) {
   const addToQueue = document.querySelector(".modal__btn-2add")
   const removeFromWatched = document.querySelector(".modal__btn-1remove")
   const removeFromQueue = document.querySelector(".modal__btn-2remove")
-  addToWatched.addEventListener('click', () => {
+  
+  
+  // ФУНКЦИИ ДОБАВЛЕНИЯ И ПЕРЕЗАПИСИ В LOCALSTORAGE
+  
+  addToWatched.addEventListener('click', async () => {
+    const data = await movie.fetchById(movieId)
+    const result = storage.readItem("watched", []);
+    const parsing = storage.readItem('qu');
+    if (parsing) {
+      const movieTitle = data.title;
+      const checkMovie = parsing.findIndex(option => option.title === movieTitle)
+      if (checkMovie >= 0) {
+      const removMovie = parsing.splice(checkMovie, 1)
+       storage.addItem("qu", parsing)
+      result.push(data);
+      storage.addItem("watched", result);
+      } else if (checkMovie === -1) {
+      result.push(data);
+      storage.addItem("watched", result);
+      }
+    } else {
+      result.push(data);
+      storage.addItem("watched", result);
+    }
     addToWatched.classList.add('hide-btn')
     removeFromWatched.classList.remove('hide-btn')
   })
-  addToQueue.addEventListener('click', () => {
+
+  addToQueue.addEventListener('click', async () => {
+    const data = await movie.fetchById(movieId)
+    const result = storage.readItem("qu", []);
+    const parsing = storage.readItem('watched');
+    if (parsing) {
+      const movieTitle = data.title;
+      const checkMovie = parsing.findIndex(option => option.title === movieTitle)
+      if (checkMovie >= 0) {
+      const removMovie = parsing.splice(checkMovie, 1)
+       storage.addItem("watched", parsing)
+      result.push(data);
+      storage.addItem("qu", result);
+      } else if (checkMovie === -1) {
+      result.push(data);
+       storage.addItem("qu", result);
+      }
+    } else {
+      result.push(data);
+      storage.addItem("qu", result);
+    }
     addToQueue.classList.add('hide-btn')
     removeFromQueue.classList.remove('hide-btn')
   })
+
+ // ФУНКЦИИ УДАЛЕНИЯ ИЗ LOCALSTORAGE
+
   removeFromWatched.addEventListener('click', () => {
+      const parsing = storage.readItem('watched');
+  const movieTitle = data.title
+    const checkMovie = parsing.findIndex(option => option.title === movieTitle)
+  if (checkMovie === -1) {
+    console.log("error")
+  }else {
+    const removMovie = parsing.splice(checkMovie, 1)
+    storage.addItem("watched", parsing);
+    } 
     removeFromWatched.classList.add('hide-btn')
     addToWatched.classList.remove('hide-btn')
   })
+  
   removeFromQueue.addEventListener('click', () => {
+      const parsing = storage.readItem('qu');
+  const movieTitle = data.title
+    const checkMovie = parsing.findIndex(option => option.title === movieTitle)
+  if (checkMovie === -1) {
+    console.log("error")
+  }else {
+    const removMovie = parsing.splice(checkMovie, 1)
+    storage.addItem("qu", parsing);
+    } 
     removeFromQueue.classList.add('hide-btn')
     addToQueue.classList.remove('hide-btn')
   })
---
+++ src/js/storage.js
@@ -0,0 +1,14 @@
+export const storage = {
+  readItem(key, defaultValue) {
+    try {
+      const result = JSON.parse(localStorage.getItem(key));
+      return result ? result : defaultValue;
+    } catch (error) {
+      return null;
+    }
+  },
+
+  addItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+};
