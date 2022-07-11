55c0a13fc18d431b23eec62af8def9743217a5f5
Andrey Ivanov <92923368+86andrey@users.noreply.github.com>
2022-07-11 12:33:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const advertBtnClose = () => {
 };
 advertCloseBtn.addEventListener('click', advertBtnClose);
 setTimeout(
-  () => advertCloseBtn.removeEventListener('click', advertBtnClose),
+  () => {advertCloseBtn.removeEventListener('click', advertBtnClose)},
   5000
 );
 
