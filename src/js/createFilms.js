b8843664cd985e07d29375140bfcec967e052e13
Renat <72465313+teenrage-dev@users.noreply.github.com>
2022-07-14 00:11:43 +0300

    Fix:)

--- src/js/createFilms.js
+++ src/js/createFilms.js
@@ -2,8 +2,6 @@ import MovieApiService from './movieFetch';
 import { renderMarkup, showRequestError } from './renderFunctions';
 import { createPagination, paginationSettings } from './pagination';
 import { HOME_SEARCH_TYPE, QUERY_SEARCH_TYPE } from './searchTypes';
-import { input } from '../index';
-
 const movie = new MovieApiService();
 
 // рендерит карточки популярных фильмов
@@ -21,29 +19,33 @@ export async function createFilmsByPopular() {
     console.dir(err);
   }
 }
+
+// Якщо пошук по запиту не прайшовав нічого, то показує популярні фільми
+
 // рендерит карточки фильмов по запросу
 export async function createFilmsBySearch(searchQuery) {
   const paginationEl = document.querySelector('#tui-pagination-container');
   const containerCard = document.querySelector('.container-card');
-
+  // console.log(optionBySearch.searchType);
   try {
     const movies = await movie.fetchByQuery(searchQuery);
 
     paginationEl.innerHTML = '';
     if (movies.results.length === 0) {
-      // paginationEl.innerHTML = '';
       showRequestError();
+      return;
     }
-    // console.log(movies);
     if (movies.total_results > 20) {
       createPagination(movies);
       paginationSettings.searchType = QUERY_SEARCH_TYPE;
     }
+    // optionBySearch.searchType = POPULAR_SEARCH_TYPE;
     renderMarkup(movies);
   } catch (err) {
-    console.log('ошибка поиска', err);
+    // console.log(err);
     showRequestError();
     paginationEl.innerHTML = '';
-    containerCard.innerHTML = '';
+    // containerCard.innerHTML = '';
+    createFilmsByPopular();
   }
 }
